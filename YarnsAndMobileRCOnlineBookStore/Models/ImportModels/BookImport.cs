1c458b8cb1b7e1fb3ddf80a74b38db9b39397e14
Roel Crodua <roelcrodua@yahoo.com>
2021-01-22 23:05:30 -0500

    Added Some properties that match to the data

---
+++ YarnsAndMobileRCOnlineBookStore/Models/ImportModels/BookImport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YarnsAndMobileRCOnlineBookStore.Models.ImportModels
+{
+    public class BookImport
+    {
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public string AuthorFirstName { get; set; }
+        public string AuthorLastName { get; set; }
+        public int? Year { get; set; }
+        public Decimal Price { get; set; }
+        public int Id { get; set; }
+    }
+}
