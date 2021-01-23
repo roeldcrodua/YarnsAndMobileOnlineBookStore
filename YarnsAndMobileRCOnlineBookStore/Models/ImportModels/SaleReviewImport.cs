a8c2d9f27b2ecb5d9e6b05580107130a9b095f79
Roel Crodua <roelcrodua@yahoo.com>
2021-01-22 23:05:53 -0500

    Added properties that matches the data

---
+++ YarnsAndMobileRCOnlineBookStore/Models/ImportModels/SaleReviewImport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YarnsAndMobileRCOnlineBookStore.Views.Admin
+{
+    public class SaleReviewImport
+    {
+        public int BookId { get; set; }
+        public int MemberId { get; set; }
+        public DateTime? SaleDate { get; set; }
+        public decimal? SalePrice { get; set; }
+        public DateTime? ReviewDate { get; set; }
+        public string ReviewTitle { get; set; }
+        public string ReviewBody { get; set; }
+        public int? ReviewRating { get; set; }
+    }
+}
