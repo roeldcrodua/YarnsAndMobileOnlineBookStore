e0e54b15e44e6bfb2165936cf08352c6f5f3a608
Roel Crodua <roelcrodua@yahoo.com>
2021-02-12 01:44:29 -0500

    Fix the Stable Genius files

--- YarnsAndMobileRCOnlineBookStore/Controllers/ContactController.cs
+++ YarnsAndMobileRCOnlineBookStore/Controllers/ContactController.cs
@@ -6,13 +6,11 @@ namespace YarnsAndMobileRCOnlineBookStore.Controllers
     public class ContactController : Controller
     {
 
-        [Route("Contact")]
         public IActionResult Contact()
         {
             return View();
         }
 
-        [Route("About")]
         public IActionResult About()
         {
             return View();
